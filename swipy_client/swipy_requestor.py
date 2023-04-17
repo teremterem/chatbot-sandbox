0373813958378a54306f2bb90031b54de9cdc15f
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-18 00:55:00 +0300

    swipy_client

--- swipy_client/swipy_requestor.py
+++ swipy_client/swipy_requestor.py
@@ -12,42 +12,42 @@ _RECV_QUEUE = asyncio.Queue()
 
 async def log_text_completion_request(caller_name: str, args: tuple[Any, ...], kwargs: dict[str, Any]) -> int:
     """Log a text completion request to Swipy Platform."""
-    request = {
+    data = {
         "_swipy_caller_name": caller_name,
         **kwargs,
     }
     if args:
-        request["_swipy_args"] = args
+        data["_swipy_args"] = args
 
-    await _send_data(request)
-    result = await _recv_data()
-    return result["text_completion_id"]
+    response = await _call_websocket(data)
+    return response["text_completion_id"]
 
 
-async def log_text_completion_response(text_completion_id: int, response: dict[str, Any]) -> None:
+async def log_text_completion_response(text_completion_id: int, completion_response: dict[str, Any]) -> None:
     """Log a text completion response to Swipy Platform."""
     data = {
         "text_completion_id": text_completion_id,
-        "response": response,
+        "response": completion_response,
     }
-    await _send_data(data)
+    await _call_websocket(data)
 
 
-async def _send_data(data: Any) -> None:
+async def _call_websocket(data: Any) -> Any:
+    # send data
     data = json.dumps(data)
     await _SEND_QUEUE.put(data)
-
-
-async def _recv_data() -> Any:
-    data = await _RECV_QUEUE.get()
-    return json.loads(data)
+    # receive response
+    response = await _RECV_QUEUE.get()
+    return json.loads(response)
 
 
 async def _websocket_client() -> None:
     async with websockets.connect(_SWIPY_WEBSOCKET_URI) as websocket:  # pylint: disable=no-member
         while True:
+            # send data
             data_to_send = await _SEND_QUEUE.get()
             await websocket.send(data_to_send)
+            # receive response
             data = await websocket.recv()
             await _RECV_QUEUE.put(data)
 
