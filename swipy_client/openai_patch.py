99df1b3c60a26af2365d55c28a0442ee6379f11d
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-17 23:54:40 +0300

    swipy_client

--- swipy_client/openai_patch.py
+++ swipy_client/openai_patch.py
@@ -11,14 +11,12 @@ from swipy_client.swipy_requestor import (
 
 def patch_openai() -> None:
     """Patch openai to log requests to Swipy Platform."""
-    chat_completion_create_original = openai.ChatCompletion.create
+    chat_completion_acreate_original = openai.ChatCompletion.acreate
 
-    def swipy_chat_completion_create(*args, **kwargs) -> dict[str, Any]:
-        text_completion_id = log_text_completion_request(
-            "openai.ChatCompletion.create", args, kwargs
-        )
-        response = chat_completion_create_original(*args, **kwargs)
-        log_text_completion_response(text_completion_id, response)
+    async def swipy_chat_completion_acreate(*args, **kwargs) -> dict[str, Any]:
+        text_completion_id = await log_text_completion_request("openai.ChatCompletion.create", args, kwargs)
+        response = await chat_completion_acreate_original(*args, **kwargs)
+        await log_text_completion_response(text_completion_id, response)
         return response
 
-    openai.ChatCompletion.create = swipy_chat_completion_create
+    openai.ChatCompletion.acreate = swipy_chat_completion_acreate
--- swipy_client/swipy_requestor.py
+++ swipy_client/swipy_requestor.py
@@ -1,36 +1,55 @@
 """This module is responsible for sending requests to Swipy Platform."""
+import asyncio
+import json
 from typing import Any
 
-import httpx
+import websockets
 
-_swipy_platform_url = "http://localhost:8000"
+_SWIPY_WEBSOCKET_URI = "ws://localhost:8000/swipy_bot_websocket/"
+_SEND_QUEUE = asyncio.Queue()
+_RECV_QUEUE = asyncio.Queue()
 
-_client = httpx.Client()
 
-
-def log_text_completion_request(
-    caller_name: str, args: tuple[Any, ...], kwargs: dict[str, Any]
-) -> int:
+async def log_text_completion_request(caller_name: str, args: tuple[Any, ...], kwargs: dict[str, Any]) -> int:
     """Log a text completion request to Swipy Platform."""
-    payload = {
+    request = {
         "_swipy_caller_name": caller_name,
         **kwargs,
     }
     if args:
-        payload["_swipy_args"] = args
+        request["_swipy_args"] = args
 
-    response = _client.post(
-        f"{_swipy_platform_url}/text_completion_request/",
-        json=payload,
-    )
-    return response.json()["text_completion_id"]
+    await _send_data(request)
+    result = await _recv_data()
+    return result["text_completion_id"]
 
 
-def log_text_completion_response(
-    text_completion_id: int, response: dict[str, Any]
-) -> None:
+async def log_text_completion_response(text_completion_id: int, response: dict[str, Any]) -> None:
     """Log a text completion response to Swipy Platform."""
-    _client.post(
-        f"{_swipy_platform_url}/text_completion_response/{text_completion_id}/",
-        json=response,
-    )
+    data = {
+        "text_completion_id": text_completion_id,
+        "response": response,
+    }
+    await _send_data(data)
+
+
+async def _send_data(data: Any) -> None:
+    data = json.dumps(data)
+    await _SEND_QUEUE.put(data)
+
+
+async def _recv_data() -> Any:
+    data = await _RECV_QUEUE.get()
+    return json.loads(data)
+
+
+async def _websocket_client() -> None:
+    async with websockets.connect(_SWIPY_WEBSOCKET_URI) as websocket:  # pylint: disable=no-member
+        while True:
+            data_to_send = await _SEND_QUEUE.get()
+            await websocket.send(data_to_send)
+            data = await websocket.recv()
+            await _RECV_QUEUE.put(data)
+
+
+asyncio.get_event_loop().create_task(_websocket_client())
