81c5ea1331b5a5df9105ce964d40330460290010
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-18 00:45:25 +0300

    swipy_client

--- chatbots/completion_logging_experiment.py
+++ chatbots/completion_logging_experiment.py
@@ -5,7 +5,7 @@ from langchain.schema import HumanMessage
 
 async def main() -> None:
     """Example of using the langchain library to generate a response to a prompt and log it to Swipy Platform."""
-    llm_chat = ChatOpenAI()
+    llm_chat = ChatOpenAI(temperature=1)
     llm_result = await llm_chat.agenerate(
         [[HumanMessage(content="What would be a good company name for a company that makes colorful socks?")]]
     )
