0831829ffc4947b77b812076d62cb58bc2699b37
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-20 23:59:04 +0300

    swipy_client

--- chatbots/completion_logging_experiment.py
+++ chatbots/completion_logging_experiment.py
@@ -12,11 +12,7 @@ llm_chat = ChatOpenAI(temperature=1)
 async def fulfillment_handler(fulfillment_id: int, data: dict[str, Any]) -> None:
     """Handle fulfillment requests from Swipy Platform."""
     message = data["message_text"]
-    print()
-    print(message)
     llm_result = await llm_chat.agenerate([[HumanMessage(content=message)]])
-    print()
-    print(llm_result.generations[0][0].text)
     await send_message(fulfillment_id, text=llm_result.generations[0][0].text)
 
 
