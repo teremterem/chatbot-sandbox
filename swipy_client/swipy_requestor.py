1918f2d68902045da87bb1845ca0df38aa325d6f
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-20 09:32:33 +0300

    swipy_client

--- swipy_client/swipy_requestor.py
+++ swipy_client/swipy_requestor.py
@@ -41,21 +41,29 @@ async def send_message(fulfillment_id: int, **data) -> None:
     _client.post(f"{_SWIPY_PLATFORM_HTTP_URL}/send_message/{fulfillment_id}/", json=data)
 
 
+async def _fulfillment_handler_wrapper(
+    fulfillment_handler: callable, fulfillment_id: int, data: dict[str, Any]
+) -> None:
+    """Wrapper for fulfillment handlers to catch exceptions and report them to the user."""
+    # pylint: disable=broad-exception-caught
+    try:
+        await fulfillment_handler(fulfillment_id, data)
+    except Exception as exc:
+        logger.exception(exc)
+        try:
+            await send_message(fulfillment_id, text=f"⚠️ Oops, something went wrong ⚠️\n\n{exc}")
+        except Exception as exc2:
+            # TODO do we even need to log this ? it may confuse what was the original error
+            logger.warning("FAILED TO REPORT ERROR TO THE USER", exc_info=exc2)
+
+
 async def run_fulfillment_client(fulfillment_handler: callable) -> None:
     """Connect to Swipy Platform and listen for fulfillment requests."""
-    # pylint: disable=no-member,broad-exception-caught
+    # pylint: disable=no-member
     # TODO reconnect if connection is lost ? how many times ? exponential backoff ?
     async with websockets.connect(f"{_SWIPY_PLATFORM_WS_URL}/fulfillment_websocket/") as websocket:
         while True:
             data_str = await websocket.recv()
-            try:
-                data = json.loads(data_str)
-                fulfillment_id = data.pop("fulfillment_id")
-                asyncio.create_task(fulfillment_handler(fulfillment_id, data))
-            except Exception as exc:
-                logger.exception(exc)
-                try:
-                    await send_message(fulfillment_id, text=f"⚠️ Oops, something went wrong ⚠️\n\n{exc}")
-                except Exception as exc2:
-                    # TODO do we even need to log this ? it may confuse what was the original error
-                    logger.warning("FAILED TO REPORT ERROR TO THE USER", exc_info=exc2)
+            data = json.loads(data_str)
+            fulfillment_id = data.pop("fulfillment_id")
+            asyncio.create_task(_fulfillment_handler_wrapper(fulfillment_handler, fulfillment_id, data))
