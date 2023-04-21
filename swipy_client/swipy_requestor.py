04f250e7a2a5af7fd73e9e169a85b1d96f1e056a
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-21 22:22:18 +0300

    remote-swipy-bots

--- swipy_client/swipy_requestor.py
+++ swipy_client/swipy_requestor.py
@@ -3,18 +3,19 @@ import asyncio
 import json
 import logging
 import os
+from contextvars import ContextVar
 from typing import Any
 
 import httpx
 import websockets
 
-SWIPY_PLATFORM_HOST = os.getenv("SWIPY_PLATFORM_HOST", "localhost:8000")
-
-SWIPY_PLATFORM_HTTP_URI = os.getenv("SWIPY_PLATFORM_HTTP_URI", "http://localhost:8000")
-SWIPY_PLATFORM_WS_URI = os.getenv("SWIPY_PLATFORM_WS_URI", "ws://localhost:8000")
+swipy_platform_http_uri = os.getenv("SWIPY_PLATFORM_HTTP_URI", "http://localhost:8000")
+swipy_platform_ws_uri = os.getenv("SWIPY_PLATFORM_WS_URI", "ws://localhost:8000")
 
 _client = httpx.Client()
 logger = logging.getLogger(__name__)
+
+_fulfillment_id_var: ContextVar[int | None] = ContextVar("fulfillment_id", default=None)
 
 
 async def log_llm_request(caller_name: str, args: tuple[Any, ...], kwargs: dict[str, Any]) -> int:
@@ -26,13 +27,17 @@ async def log_llm_request(caller_name: str, args: tuple[Any, ...], kwargs: dict[
     if args:
         data["_swipy_args"] = args
 
-    response = _client.post(f"{SWIPY_PLATFORM_HTTP_URI}/log_llm_request/", json=data)
+    fulfillment_id = _fulfillment_id_var.get()
+    if fulfillment_id is not None:
+        data["_swipy_fulfillment_id"] = fulfillment_id
+
+    response = _client.post(f"{swipy_platform_http_uri}/log_llm_request/", json=data)
     return response.json()["llm_request_id"]
 
 
 async def log_llm_response(llm_request_id: int, llm_response: dict[str, Any]) -> None:
     """Log a LLM response to Swipy Platform."""
-    _client.post(f"{SWIPY_PLATFORM_HTTP_URI}/log_llm_response/{llm_request_id}/", json=llm_response)
+    _client.post(f"{swipy_platform_http_uri}/log_llm_response/{llm_request_id}/", json=llm_response)
 
 
 async def send_message(fulfillment_id: int, **data) -> None:
@@ -40,7 +45,7 @@ async def send_message(fulfillment_id: int, **data) -> None:
     Send a message from a chatbot on Swipy Platform. Which chatbot, which chat, etc. is determined by the
     fulfillment_id.
     """
-    _client.post(f"{SWIPY_PLATFORM_HTTP_URI}/send_message/{fulfillment_id}/", json=data)
+    _client.post(f"{swipy_platform_http_uri}/send_message/{fulfillment_id}/", json=data)
 
 
 async def _fulfillment_handler_wrapper(
@@ -49,6 +54,7 @@ async def _fulfillment_handler_wrapper(
     """Wrapper for fulfillment handlers to catch exceptions and report them to the user."""
     # pylint: disable=broad-exception-caught
     try:
+        _fulfillment_id_var.set(fulfillment_id)
         await fulfillment_handler(fulfillment_id, data)
     except Exception as exc:
         logger.exception(exc)
@@ -63,7 +69,7 @@ async def run_fulfillment_client(fulfillment_handler: callable) -> None:
     """Connect to Swipy Platform and listen for fulfillment requests."""
     # pylint: disable=no-member
     # TODO reconnect if connection is lost ? how many times ? exponential backoff ?
-    async with websockets.connect(f"{SWIPY_PLATFORM_WS_URI}/fulfillment_websocket/") as websocket:
+    async with websockets.connect(f"{swipy_platform_ws_uri}/fulfillment_websocket/") as websocket:
         while True:
             data_str = await websocket.recv()
             data = json.loads(data_str)
