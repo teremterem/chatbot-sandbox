3114e5fc7115603f79c4cc8872eea27f0f379d9d
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-23 00:06:07 +0300

    remote-swipy-bots

--- swipy_client/openai_patch.py
+++ swipy_client/openai_patch.py
@@ -11,9 +11,14 @@ def patch_openai() -> None:
     _chat_completion_acreate_original = openai.ChatCompletion.acreate
 
     async def _chat_completion_acreate(*args, **kwargs) -> dict[str, Any]:
+        # TODO do asyncio.create_task() instead of await ?
         llm_request_id = await log_llm_request("openai.ChatCompletion.create", args, kwargs)
+
         response = await _chat_completion_acreate_original(*args, **kwargs)
+
+        # TODO do asyncio.create_task() instead of await ?
         await log_llm_response(llm_request_id, response)
+
         return response
 
     openai.ChatCompletion.acreate = _chat_completion_acreate
