a19664e4b4c1213b7b87066978eec9e9ed932348
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-24 16:28:26 +0300

    remote-swipy-bots

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 """Entry point for the chatbot sandbox."""
 # pylint: disable=wrong-import-position
 import asyncio
+import os
 
 from dotenv import load_dotenv
 
@@ -10,7 +11,16 @@ from swipy_client import patch_openai
 
 patch_openai()
 
-from chatbots.pdf_bot import main
+from chatbots.pdf_bot import PdfBot
+
+
+async def main() -> None:
+    """Run the chatbot sandbox."""
+    pdf_bot = asyncio.create_task(
+        PdfBot(os.environ["PDF_BOT_TOKEN"], "2023_GPT4All_Technical_Report.pdf").run_fulfillment_client()
+    )
+    await asyncio.gather(pdf_bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- chatbots/pdf_bot.py
+++ chatbots/pdf_bot.py
@@ -2,7 +2,6 @@
 This chatbot is based on materials from the following video by Prompt Engineering YouTube channel
 (checkout video description for more links): https://www.youtube.com/watch?v=TLf90ipMzfE
 """
-import os
 from typing import Any
 
 from PyPDF2 import PdfReader
@@ -14,59 +13,55 @@ from langchain.text_splitter import CharacterTextSplitter
 
 from swipy_client import SwipyBot
 
-ANTI_SWIPY_BOT_TOKEN = os.environ["ANTI_SWIPY_BOT_TOKEN"]
 
+class PdfBot:
+    """A chatbot that answers questions about a PDF document."""
 
-async def pdf_bot_handler(bot: SwipyBot, data: dict[str, Any]) -> None:
-    """Handle fulfillment requests from Swipy Platform."""
-    message = data["message"]
-    query = message["content"]
-    print("USER:", query)
+    def __init__(self, swipy_bot_token: str, pdf_filename: str) -> None:
+        self.swipy_bot_token = swipy_bot_token
+        self.docsearch = self._ingest_pdf(pdf_filename)
 
-    # llm_context = [ChatMessage(role=msg["role"], content=msg["content"]) for msg in data["message_history"]]
-    # llm_context.append(ChatMessage(role=message["role"], content=query))
+    async def run_fulfillment_client(self):
+        """Connect to Swipy Platform and listen for fulfillment requests."""
+        await SwipyBot(self.swipy_bot_token).run_fulfillment_client(self._fulfillment_handler)
 
-    llm_chat = ChatOpenAI(user=data["user_uuid"])
-    chain = load_qa_chain(llm_chat, chain_type="stuff")
-    docs = docsearch.similarity_search(query)
-    response = await chain.arun(input_documents=docs, question=query)
+    async def _fulfillment_handler(self, bot: SwipyBot, data: dict[str, Any]) -> None:
+        """Handle fulfillment requests from Swipy Platform."""
+        message = data["message"]
+        query = message["content"]
+        print("USER:", query)
 
-    print("BOT:", response)
-    print()
-    await bot.send_message(text=response)
+        # llm_context = [ChatMessage(role=msg["role"], content=msg["content"]) for msg in data["message_history"]]
+        # llm_context.append(ChatMessage(role=message["role"], content=query))
 
+        llm_chat = ChatOpenAI(user=data["user_uuid"])
+        chain = load_qa_chain(llm_chat, chain_type="stuff")
+        docs = self.docsearch.similarity_search(query)
+        response = await chain.arun(input_documents=docs, question=query)
 
-def ingest_pdf(filename: str) -> FAISS:
-    """Ingest a PDF and return a FAISS instance."""
-    reader = PdfReader(filename)
-    raw_text_parts = [page.extract_text() for page in reader.pages]
-    raw_text = "\n".join(raw_text_parts)
-    # print()
-    # print()
-    # print(raw_text)
-    # print()
-    # print()
-
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len,
-    )
-    texts = text_splitter.split_text(raw_text)
-    for text in texts:
-        print(len(text))
-        print(text)
+        print("BOT:", response)
         print()
-        print()
+        await bot.send_message(text=response)
 
-    embeddings = OpenAIEmbeddings()
-    return FAISS.from_texts(texts, embeddings)
+    @staticmethod
+    def _ingest_pdf(pdf_filename: str) -> FAISS:
+        """Ingest a PDF and return a FAISS instance."""
+        reader = PdfReader(pdf_filename)
+        raw_text_parts = [page.extract_text() for page in reader.pages]
+        raw_text = "\n".join(raw_text_parts)
 
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        texts = text_splitter.split_text(raw_text)
+        for text in texts:
+            print(len(text))
+            print(text)
+            print()
+            print()
 
-docsearch = ingest_pdf("2023_GPT4All_Technical_Report.pdf")
-
-
-async def main() -> None:
-    """Example of using the langchain library to generate a response to a prompt and log it to Swipy Platform."""
-    await SwipyBot(ANTI_SWIPY_BOT_TOKEN).run_fulfillment_client(pdf_bot_handler)
+        embeddings = OpenAIEmbeddings()
+        return FAISS.from_texts(texts, embeddings)
