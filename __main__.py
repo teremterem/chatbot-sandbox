5e6c273835cf9c49de5e7fe41b6730eb2bb92dcc
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-24 22:12:51 +0300

    remote-swipy-bots

--- __main__.py
+++ __main__.py
@@ -11,18 +11,25 @@ from swipy_client import patch_openai
 
 patch_openai()
 
-from chatbots.pdf_bot import PdfBot
+from chatbots.talk_to_doc import TalkToDocBot, pdf_to_faiss
 
 
 async def main() -> None:
     """Run the chatbot sandbox."""
-    gpt4all_pdf_bot = asyncio.create_task(
-        PdfBot(os.environ["GPT4ALL_PDF_BOT_TOKEN"], "2023_GPT4All_Technical_Report.pdf").run_fulfillment_client()
+    await asyncio.gather(
+        asyncio.create_task(
+            TalkToDocBot(
+                os.environ["GPT4ALL_PDF_BOT_TOKEN"],
+                pdf_to_faiss("2023_GPT4All_Technical_Report.pdf"),
+            ).run_fulfillment_client()
+        ),
+        asyncio.create_task(
+            TalkToDocBot(
+                os.environ["INSTRUCT_GPT_PDF_BOT_TOKEN"],
+                pdf_to_faiss("2203.02155.pdf"),
+            ).run_fulfillment_client()
+        ),
     )
-    instruct_gpt_pdf_bot = asyncio.create_task(
-        PdfBot(os.environ["INSTRUCT_GPT_PDF_BOT_TOKEN"], "2203.02155.pdf").run_fulfillment_client()
-    )
-    await asyncio.gather(gpt4all_pdf_bot, instruct_gpt_pdf_bot)
 
 
 if __name__ == "__main__":
--- chatbots/talk_to_doc.py
+++ chatbots/talk_to_doc.py
@@ -11,16 +11,17 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import VectorStore
 
 from swipy_client import SwipyBot
 
 
-class PdfBot:
+class TalkToDocBot:
     """A chatbot that answers questions about a PDF document."""
 
-    def __init__(self, swipy_bot_token: str, pdf_filename: str) -> None:
+    def __init__(self, swipy_bot_token: str, vector_store: VectorStore) -> None:
         self.swipy_bot_token = swipy_bot_token
-        self.docsearch = self._ingest_pdf(pdf_filename)
+        self.vector_store = vector_store
 
     async def run_fulfillment_client(self):
         """Connect to Swipy Platform and listen for fulfillment requests."""
@@ -35,7 +36,7 @@ class PdfBot:
 
         llm_chat = ChatOpenAI(user=data["user_uuid"])
         chain = load_qa_chain(llm_chat, chain_type="stuff")
-        docs = self.docsearch.similarity_search(query)
+        docs = self.vector_store.similarity_search(query)
         response = await chain.arun(
             input_documents=docs,
             question=query,
@@ -60,25 +61,24 @@ class PdfBot:
         query_parts.append("ASSISTANT:")
         return "".join(query_parts)
 
-    @staticmethod
-    def _ingest_pdf(pdf_filename: str) -> FAISS:
-        """Ingest a PDF and return a FAISS instance."""
-        reader = PdfReader(pdf_filename)
-        raw_text_parts = [page.extract_text() for page in reader.pages]
-        raw_text = "\n".join(raw_text_parts)
 
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len,
-        )
-        texts = text_splitter.split_text(raw_text)
-        for text in texts:
-            print(len(text))
-            print(text)
-            print()
-            print()
+def pdf_to_faiss(pdf_filename: str) -> FAISS:
+    """Ingest a PDF and return a FAISS instance."""
+    reader = PdfReader(pdf_filename)
+    raw_text_parts = [page.extract_text() for page in reader.pages]
+    raw_text = "\n".join(raw_text_parts)
 
-        embeddings = OpenAIEmbeddings()
-        return FAISS.from_texts(texts, embeddings)
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+    texts = text_splitter.split_text(raw_text)
+    for text in texts:
+        print(text)
+        print("LENGTH:", len(text), "CHARS")
+        print()
+
+    embeddings = OpenAIEmbeddings()
+    return FAISS.from_texts(texts, embeddings)
