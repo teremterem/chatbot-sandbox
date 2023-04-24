c24572d5a97335c2558d78ec85717e531f7d87e9
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-24 23:52:42 +0300

    remote-swipy-bots

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 """Entry point for the chatbot sandbox."""
 # pylint: disable=wrong-import-position
 import asyncio
+import os
 
 from dotenv import load_dotenv
 
@@ -10,26 +11,31 @@ from swipy_client import patch_openai
 
 patch_openai()
 
-from chatbots.talk_to_doc import repo_to_faiss
+from chatbots.talk_to_doc import TalkToDocBot, repo_to_faiss
 
 
 async def main() -> None:
     """Run the chatbot sandbox."""
-    repo_to_faiss(".")
-    # await asyncio.gather(
-    #     asyncio.create_task(
-    #         TalkToDocBot(
-    #             os.environ["GPT4ALL_PDF_BOT_TOKEN"],
-    #             pdf_to_faiss("2023_GPT4All_Technical_Report.pdf"),
-    #         ).run_fulfillment_client()
-    #     ),
-    #     # asyncio.create_task(
-    #     #     TalkToDocBot(
-    #     #         os.environ["INSTRUCT_GPT_PDF_BOT_TOKEN"],
-    #     #         pdf_to_faiss("2203.02155.pdf"),
-    #     #     ).run_fulfillment_client()
-    #     # ),
-    # )
+    await asyncio.gather(
+        asyncio.create_task(
+            TalkToDocBot(
+                os.environ["ANTI_SWIPY_BOT_TOKEN"],
+                repo_to_faiss("."),
+            ).run_fulfillment_client()
+        ),
+        # asyncio.create_task(
+        #     TalkToDocBot(
+        #         os.environ["ANTI_SWIPY_BOT_TOKEN"],
+        #         pdf_to_faiss("2023_GPT4All_Technical_Report.pdf"),
+        #     ).run_fulfillment_client()
+        # ),
+        # asyncio.create_task(
+        #     TalkToDocBot(
+        #         os.environ["TEREMS_BOT_TOKEN"],
+        #         pdf_to_faiss("2203.02155.pdf"),
+        #     ).run_fulfillment_client()
+        # ),
+    )
 
 
 if __name__ == "__main__":
