bd45a3ca82048f6406d499daf4aa491beec4f2c8
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-24 23:50:06 +0300

    remote-swipy-bots

--- __main__.py
+++ __main__.py
@@ -1,7 +1,6 @@
 """Entry point for the chatbot sandbox."""
 # pylint: disable=wrong-import-position
 import asyncio
-import os
 
 from dotenv import load_dotenv
 
@@ -11,25 +10,26 @@ from swipy_client import patch_openai
 
 patch_openai()
 
-from chatbots.talk_to_doc import TalkToDocBot, pdf_to_faiss
+from chatbots.talk_to_doc import repo_to_faiss
 
 
 async def main() -> None:
     """Run the chatbot sandbox."""
-    await asyncio.gather(
-        asyncio.create_task(
-            TalkToDocBot(
-                os.environ["GPT4ALL_PDF_BOT_TOKEN"],
-                pdf_to_faiss("2023_GPT4All_Technical_Report.pdf"),
-            ).run_fulfillment_client()
-        ),
-        # asyncio.create_task(
-        #     TalkToDocBot(
-        #         os.environ["INSTRUCT_GPT_PDF_BOT_TOKEN"],
-        #         pdf_to_faiss("2203.02155.pdf"),
-        #     ).run_fulfillment_client()
-        # ),
-    )
+    repo_to_faiss(".")
+    # await asyncio.gather(
+    #     asyncio.create_task(
+    #         TalkToDocBot(
+    #             os.environ["GPT4ALL_PDF_BOT_TOKEN"],
+    #             pdf_to_faiss("2023_GPT4All_Technical_Report.pdf"),
+    #         ).run_fulfillment_client()
+    #     ),
+    #     # asyncio.create_task(
+    #     #     TalkToDocBot(
+    #     #         os.environ["INSTRUCT_GPT_PDF_BOT_TOKEN"],
+    #     #         pdf_to_faiss("2203.02155.pdf"),
+    #     #     ).run_fulfillment_client()
+    #     # ),
+    # )
 
 
 if __name__ == "__main__":
--- chatbots/talk_to_doc.py
+++ chatbots/talk_to_doc.py
@@ -3,6 +3,8 @@ This chatbot is based on materials from the following video by Prompt Engineerin
 (checkout video description for more links): https://www.youtube.com/watch?v=TLf90ipMzfE
 """
 import itertools
+import os
+from pathlib import Path
 from typing import Any
 
 from PyPDF2 import PdfReader
@@ -12,6 +14,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import VectorStore
+from pathspec import pathspec
 
 from swipy_client import SwipyBot
 
@@ -82,3 +85,68 @@ def pdf_to_faiss(pdf_filename: str) -> FAISS:
 
     embeddings = OpenAIEmbeddings()
     return FAISS.from_texts(texts, embeddings)
+
+
+def repo_to_faiss(repo_path: str) -> FAISS:
+    """Ingest a git repository and return a FAISS instance."""
+    filepaths = _list_files_in_repo(repo_path)
+    print()
+    for filepath in filepaths:
+        print(filepath)
+    print()
+    print("TOTAL:", len(filepaths), "FILES")
+    print()
+    print("================================================================================")
+    print()
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+    texts = []
+    for filepath in filepaths:
+        with open(filepath, "r", encoding="utf-8") as file:
+            raw_text = file.read()
+
+        texts.extend(text_splitter.split_text(raw_text))
+
+    for text in texts:
+        print(text)
+        print()
+        print("================================================================================")
+        print()
+
+    embeddings = OpenAIEmbeddings()
+    return FAISS.from_texts(texts, embeddings)
+
+
+def _list_files_in_repo(repo_path: str | Path) -> list[Path]:
+    repo_path = Path(repo_path)
+
+    gitignore_content = ".*\n" + _read_gitignore(repo_path)
+    spec = pathspec.PathSpec.from_lines("gitwildmatch", gitignore_content.splitlines())
+
+    files_list = []
+    for root, dirs, files in os.walk(repo_path):
+        root = Path(root)
+        # Remove excluded directories from the list to prevent os.walk from processing them
+        dirs[:] = [d for d in dirs if not spec.match_file(root / d)]
+
+        for file in files:
+            file_path = root / file
+            if not spec.match_file(file_path):
+                files_list.append(file_path.relative_to(repo_path))
+
+    return files_list
+
+
+def _read_gitignore(repo_path: str | Path) -> str:
+    gitignore_path = os.path.join(repo_path, ".gitignore")
+    if not os.path.isfile(gitignore_path):
+        return ""
+
+    with open(gitignore_path, "r", encoding="utf-8") as file:
+        gitignore_content = file.read()
+    return gitignore_content
