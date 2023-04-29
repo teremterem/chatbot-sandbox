ed15dde0a4125ad41b39e4faa71e3a1228a8cfa1
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-29 15:21:01 +0300

    conversational-qna

--- chatbots/langchain_customizations.py
+++ chatbots/langchain_customizations.py
@@ -107,7 +107,7 @@ class SwipyStuffDocumentsChain(StuffDocumentsChain):
         ]
         line_separator = "\n"
         await self.swipy_bot.send_message(
-            text=f"⏳_Looking at_\n{line_separator.join(doc_list)}\n",
+            text=f"_Looking at_ 🔍\n{line_separator.join(doc_list)}",
             parse_mode="Markdown",
             disable_notification=True,
             disable_web_page_preview=True,
@@ -139,7 +139,7 @@ class SwipyRefineDocumentsChain(RefineDocumentsChain):
 
     async def _report_doc_processing(self, doc: Document) -> None:
         await self.swipy_bot.send_message(
-            text=f"⏳ _Looking at_ [{self.pretty_path_prefix}{doc.metadata['path']}]({doc.metadata['source']})",
+            text=f"_Looking at_ [{self.pretty_path_prefix}{doc.metadata['path']}]({doc.metadata['source']}) 🔍",
             parse_mode="Markdown",
             disable_notification=True,
             disable_web_page_preview=True,
@@ -156,7 +156,7 @@ class ThinkingCallbackHandler(AsyncCallbackHandler):
 
     async def on_chain_start(self, serialized: dict[str, Any], inputs: dict[str, Any], **kwargs: Any) -> None:
         await self.swipy_bot.send_message(
-            text="⏳ _Thinking_",
+            text="_Thinking_ ⏳",
             parse_mode="Markdown",
             disable_notification=True,
             disable_web_page_preview=True,
