e895253198aabe62f9d9fcc082c79e063bac5ed9
Oleksandr Tereshchenko <toporok@gmail.com>
2023-05-02 23:03:17 +0300

    conversational-qna

--- scripts/run_swipy_sandbox.py
+++ scripts/run_swipy_sandbox.py
@@ -26,39 +26,60 @@ def create_langchain_experiments(exp_name_suffix: str, faiss_subfolder: str) -> 
     """Create the experiments for the langchain_src bot."""
     embeddings = get_embeddings()
 
-    langchain_src_faiss = FAISS.load_local(
-        str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_src"),
+    langchain_faiss = FAISS.load_local(
+        str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_full"),
         embeddings,
     )
-    langchain_test_faiss = FAISS.load_local(
-        str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_tests"),
+    auto_gpt_faiss = FAISS.load_local(
+        str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "auto_gpt_full"),
         embeddings,
     )
-    langchain_doc_faiss = FAISS.load_local(
-        str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_docs"),
-        embeddings,
-    )
+    # langchain_src_faiss = FAISS.load_local(
+    #     str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_src"),
+    #     embeddings,
+    # )
+    # langchain_test_faiss = FAISS.load_local(
+    #     str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_tests"),
+    #     embeddings,
+    # )
+    # langchain_doc_faiss = FAISS.load_local(
+    #     str(REPO_PATH / "data" / "faiss" / faiss_subfolder / "langchain_docs"),
+    #     embeddings,
+    # )
 
     def create_gpt_4_3_experiments(_exp_name_suffix: str, use_gpt4: bool) -> list[ConvRetrievalBot]:
-        langchain_src_bot = StuffConvRetrievalBot(
-            SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_src_{_exp_name_suffix}"),
-            langchain_src_faiss,
+        langchain_bot = StuffConvRetrievalBot(
+            SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_{_exp_name_suffix}"),
+            langchain_faiss,
             use_gpt4=use_gpt4,
             pretty_path_prefix="langchain/",
         )
-        langchain_test_bot = StuffConvRetrievalBot(
-            SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_test_{_exp_name_suffix}"),
-            langchain_test_faiss,
+        auto_gpt_bot = StuffConvRetrievalBot(
+            SwipyBot(os.environ["ANTI_SWIPY_BOT_TOKEN"], experiment_name=f"ag_{_exp_name_suffix}"),
+            auto_gpt_faiss,
             use_gpt4=use_gpt4,
-            pretty_path_prefix="langchain/tests/",
+            pretty_path_prefix="Auto-GPT/",
         )
-        langchain_doc_bot = StuffConvRetrievalBot(
-            SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_doc_{_exp_name_suffix}"),
-            langchain_doc_faiss,
-            use_gpt4=use_gpt4,
-            pretty_path_prefix="langchain/docs/",
-        )
-        return [langchain_src_bot, langchain_test_bot, langchain_doc_bot]
+        return [langchain_bot, auto_gpt_bot]
+        # langchain_src_bot = StuffConvRetrievalBot(
+        #     SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_src_{_exp_name_suffix}"),
+        #     langchain_src_faiss,
+        #     use_gpt4=use_gpt4,
+        #     pretty_path_prefix="langchain/",
+        # )
+        # langchain_test_bot = StuffConvRetrievalBot(
+        #     SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_test_{_exp_name_suffix}"),
+        #     langchain_test_faiss,
+        #     use_gpt4=use_gpt4,
+        #     pretty_path_prefix="langchain/tests/",
+        # )
+        # langchain_doc_bot = StuffConvRetrievalBot(
+        #     SwipyBot(os.environ["LANGCHAIN_SRC_BOT_TOKEN"], experiment_name=f"lc_doc_{_exp_name_suffix}"),
+        #     langchain_doc_faiss,
+        #     use_gpt4=use_gpt4,
+        #     pretty_path_prefix="langchain/docs/",
+        # )
+        # return [langchain_src_bot, langchain_test_bot, langchain_doc_bot]
 
     return [
         *create_gpt_4_3_experiments("gpt3_" + exp_name_suffix, use_gpt4=False),
@@ -71,8 +92,10 @@ async def main() -> None:
 
     # Create the bots
     bots = [
-        *create_langchain_experiments("2k", "2000-400"),
-        *create_langchain_experiments("1k", "1000-200"),
+        *create_langchain_experiments("2k", "2000-1000"),
+        *create_langchain_experiments("1k", "1000-500"),
+        # *create_langchain_experiments("2k", "2000-400"),
+        # *create_langchain_experiments("1k", "1000-200"),
     ]
     bots[0].swipy_bot.experiment_name += "-default"
 
